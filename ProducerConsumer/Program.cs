efcb702654e840c74a00cd7c07d25f21ee32e2a3
Lazizbek-Xoshimov <lazizbekaxion19@gmail.com>
2026-06-24 17:39:03 +0500

    Used List<T> with lock keyword

---
+++ ProducerConsumer/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace ProducerConsumer;
+
+public class Program
+{
+    private static List<int> numbers = new List<int>();
+
+    public static void Main(string[] args)
+    {
+        Thread Producer = new Thread(AddNumbers);
+        Thread Consumer = new Thread(RemoveAndPrintNumbers);
+
+        Producer.Start();
+        Producer.Join();
+        
+        Consumer.Start();
+    }
+
+    public static void AddNumbers()
+    {
+        lock (numbers)
+        {
+            for (int i = 0; i < 10; i ++)
+                numbers.Add(i);
+        }
+    }
+
+    public static void RemoveAndPrintNumbers()
+    {
+        lock (numbers)
+        {
+            foreach (int number in numbers)
+                Console.Write(number + " ");
+
+            numbers.Clear();
+        }
+    }
+}
