172afb54b2d69a3c1ed714dea9d1055cc694a04a
Lazizbek-Xoshimov <lazizbekaxion19@gmail.com>
2026-06-24 19:18:01 +0500

    Thread implemented with Join

---
+++ SequentialExecutionWithJoin/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace SequentialExecutionWithJoin;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Thread threadFirst = new Thread(TopTen);
+        Thread threadSecond = new Thread(SecondDecade);
+        Thread threadThird = new Thread(ThirdDecimalPlace);
+
+        threadFirst.Start();
+        threadFirst.Join();
+        
+        threadSecond.Start();
+        threadSecond.Join();
+
+        threadThird.Start();
+        threadThird.Join();        
+    }
+
+    public static void TopTen()
+    {
+        for(int i = 1; i <= 10; i ++)
+            Console.Write(i + " ");
+    }
+
+    public static void SecondDecade()
+    {
+        for(int i = 11; i <= 20; i ++)
+            Console.Write(i + " ");
+    }
+
+    public static void ThirdDecimalPlace()
+    {
+        for(int i = 21; i <= 30; i ++)
+            Console.Write(i + " ");
+    }
+}
