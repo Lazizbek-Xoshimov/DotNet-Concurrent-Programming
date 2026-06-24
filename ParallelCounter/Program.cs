d509e470f7c5e10b41e2346f883c88d709b3d645
Lazizbek-Xoshimov <lazizbekaxion19@gmail.com>
2026-06-24 15:48:33 +0500

    3 threads created and used

--- ParallelCounter/Program.cs
+++ ParallelCounter/Program.cs
@@ -1,1 +1,23 @@
-﻿Console.WriteLine("Hello, World!");
+﻿namespace ParallelCounter;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Thread threadA = new Thread(DisplayNumbers);
+        Thread threadB = new Thread(DisplayNumbers);
+        Thread threadC = new Thread(DisplayNumbers);
+
+        threadA.Start();
+        threadB.Start();
+        threadC.Start();
+    }
+
+    public static void DisplayNumbers()
+    {
+        for (int i = 1; i <= 100; i ++)
+            Console.Write(i + " ");
+
+        Thread.Sleep(2000);
+    }
+}
