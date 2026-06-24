dfd16e1812a9f075b2ba45348b7e27639935aca9
Lazizbek-Xoshimov <lazizbekaxion19@gmail.com>
2026-06-24 17:16:36 +0500

    Balance security is ensured through lock

---
+++ SharedBankAccount/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace SharedBankAccount;
+
+public class Program
+{
+    private static int balance = 1000;
+    private static readonly object balanceLock = new ();
+
+    public static void Main(string[] args)
+    {
+        Thread threadFirst = new Thread(Withdraw);
+        Thread threadSecond = new Thread(Withdraw);
+        Thread threadThird = new Thread(Withdraw);
+        Thread threadFourth = new Thread(Withdraw);
+        Thread threadFifth = new Thread(Withdraw);
+
+        threadFirst.Start();
+        threadSecond.Start();
+        threadThird.Start();
+        threadFourth.Start();
+        threadFifth.Start();
+
+        Console.WriteLine($"Current balance: {balance}");
+    }
+
+    public static void Withdraw()
+    {
+        lock (balanceLock)
+        {
+            if (balance > 0)
+                balance -= 100;
+        }
+    }
+}
