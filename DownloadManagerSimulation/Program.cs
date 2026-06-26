7ecd02a6aa4837a15250e2a65e959b70a5d25ed5
Lazizbek-Xoshimov <lazizbekaxion19@gmail.com>
2026-06-26 13:16:10 +0500

    A console application was created that downloads files in separate threads

---
+++ DownloadManagerSimulation/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace DownloadManagerSimulation;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        List<Thread> threads = new List<Thread>();
+        Random random = new Random();
+        object threadLock = new object();
+
+        Console.Clear();
+        Console.WriteLine("Files downloading...");
+
+        for (int i = 0; i < 5; i++)
+        {
+            threads.Add(new Thread(() =>
+            {
+                lock (threadLock)
+                {
+                    for (int j = 10; j <= 100; j += 10)
+                    {
+                        Console.SetCursorPosition(0, int.Parse(Thread.CurrentThread.Name) + 1);
+                        Console.WriteLine($"File {Thread.CurrentThread.Name} -> {j} %");
+                        Thread.Sleep(random.Next(1, 10) * 100);
+                    }
+                }
+            })
+            {
+                Name = $"{i}",
+                IsBackground = false
+            });
+        }
+        
+        foreach (Thread thread in threads)
+            thread.Start();
+
+        foreach (Thread thread in threads)
+            thread.Join();
+
+        Console.SetCursorPosition(0, 6);
+        Console.WriteLine("Files downloaded.");
+    }
+}
