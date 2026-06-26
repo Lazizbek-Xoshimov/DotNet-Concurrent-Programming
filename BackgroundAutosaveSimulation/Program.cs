4a474c8e8fdb7d376a74ad17d19d8024de49238d
Lazizbek-Xoshimov <lazizbekaxion19@gmail.com>
2026-06-26 12:04:52 +0500

    Background thread started in Main thread

---
+++ BackgroundAutosaveSimulation/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace BackgroundAutosaveSimulation;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Thread.CurrentThread.Name = "Main Thread";
+
+        Console.Write($"{Thread.CurrentThread.Name}: ");
+        Console.WriteLine("User is typing...");
+
+        Thread backThread = new Thread(() => 
+        {
+            Console.Write($"{Thread.CurrentThread.Name}: ");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write(i + " ");
+                Thread.Sleep(1000);
+            }
+            Console.WriteLine("Data saved.");
+
+            Console.WriteLine($"{Thread.CurrentThread.Name} has ended.");
+        })
+        {
+            Name = "Background Thread",
+            IsBackground = true
+        };
+
+        backThread.Start();
+
+        Thread.Sleep(11000);
+        Console.WriteLine($"{Thread.CurrentThread.Name} has ended.");
+    }
+}
